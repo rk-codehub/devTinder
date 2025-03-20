a0aa74f38f06daf1f658087f326bf063ca434211
rk <ranjithkumark1996work@gmail.com>
2025-03-21 01:05:58 +0530

    exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express(); // IT CREATES AN APPLICATION OF EXPRESS JS
 
+// THIS WILL ONLY HANDLE GET CALL TO /user
+app.get("/user", (req, res) => {
+    res.send({ firstname: "R", lastname: "K" })
+})
+
+app.post("/user", (req, res) => {
+    console.log("Save Data to the database")
+    res.send("Data successfully saved to the database")
+})
+
+app.delete("/user", (req, res) => {
+    console.log("Save Data to the database")
+    res.send("Data deleted successfully")
+})
+
+// THIS WILL MATCH ALL THE HTTP METHOD API CALLS TO /test
 app.use("/test",(req, res) => {
     res.send("HELLO FROM THE SERVER");
 });
