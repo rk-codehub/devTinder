f900ea2de4b54397a358328c1c0ca232cada9c6a
rk <ranjithkumark1996work@gmail.com>
2025-03-21 01:18:54 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,24 +3,11 @@ const express = require('express');
 const app = express(); // IT CREATES AN APPLICATION OF EXPRESS JS
 
 // THIS WILL ONLY HANDLE GET CALL TO /user
-app.get("/user", (req, res) => {
+
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
     res.send({ firstname: "R", lastname: "K" })
 })
-
-app.post("/user", (req, res) => {
-    console.log("Save Data to the database")
-    res.send("Data successfully saved to the database")
-})
-
-app.delete("/user", (req, res) => {
-    console.log("Save Data to the database")
-    res.send("Data deleted successfully")
-})
-
-// THIS WILL MATCH ALL THE HTTP METHOD API CALLS TO /test
-app.use("/test",(req, res) => {
-    res.send("HELLO FROM THE SERVER");
-});
 
 app.listen(3000, () => {
     console.log("SERVER IS SUCCESSFULLY LISTENING ON PORT 3000...");
